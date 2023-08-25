1c07e9932383d547fc98a6da40a9f9a9a6a12a86
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-25 17:53:16 +0530

    display data from backend

--- lib/screens/grocery_list.dart
+++ lib/screens/grocery_list.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_form_app/data/categories.dart';
 import 'package:flutter_form_app/data/dummy_items.dart';
 import 'package:flutter_form_app/data/grocery_item.dart';
 import 'package:flutter_form_app/screens/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -12,6 +16,7 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryList = [];
+
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(
@@ -46,19 +51,47 @@ class _GroceryListState extends State<GroceryList> {
           title: const Text('Groceries'),
           actions: [
             IconButton(
-                onPressed: () async {
-                  final newItem = await Navigator.of(context).push<GroceryItem>(
-                      MaterialPageRoute(builder: (builder) => NewItemScreen()));
-                  if (newItem == null) {
-                    return;
-                  }
-                  setState(() {
-                    _groceryList.add(newItem);
-                  });
+                onPressed: () {
+                  _addItem();
                 },
                 icon: Icon(Icons.add))
           ],
         ),
         body: content);
+  }
+
+  void _addItem() async {
+    await Navigator.of(context).push<GroceryItem>(
+        MaterialPageRoute(builder: (builder) => NewItemScreen()));
+
+    _loadItem();
+  }
+
+  void _loadItem() async {
+    final url = Uri.https(
+        'fir-prep-c590d-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final List<GroceryItem> _loadedItems = [];
+    final Map<String, dynamic> listData = json.decode(response.body);
+    for (final item in listData.entries) {
+      _loadedItems.add(GroceryItem(
+        id: item.key,
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: categories.entries
+            .firstWhere(
+                (element) => element.value.name == item.value['category']!)
+            .value,
+      ));
+    }
+    setState(() {
+      _groceryList = _loadedItems;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItem();
   }
 }
